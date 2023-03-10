13ed2a25e319b668d635bdfd59f7162d4eb9cff4
EUNSOLY <dmsthf9596@gmail.com>
2023-03-10 20:57:28 +0900

    23 .03.10 대전시민천문대 리뉴얼 3일차

--- js/common.js
+++ js/common.js
@@ -4,10 +4,7 @@ const $gnb_wrap = document.querySelector(".gnb_wrap");
 const $M_nav = document.querySelector(".M_nav");
 const $M_btn = document.querySelector(".M_btn");
 
-//팝업존
-const $list_img = document.querySelectorAll(".list_img");
-const $head_prev = document.querySelector(".head_prev_btn");
-const $head_next = document.querySelector(".head_next_btn");
+
 // 시설안내
 const $facility_btn = document.querySelectorAll(".facility_btn");
 const $list3_text = document.querySelectorAll(".list3_text");
@@ -32,3 +29,38 @@ $facility_btn.forEach((item, i, array) => {
     item.classList.add("active");
   });
 });
+
+//팝업존
+const $list_img = document.querySelectorAll(".list_img");
+const $head_prev = document.querySelector(".head_prev_btn");
+const $head_next = document.querySelector(".head_next_btn");
+const $popzon = document.querySelector(".popzon")
+console.log($popzon);
+
+let sliderWidth = $list_img[0].offsetWidth;
+
+window.addEventListener("resize",()=>{
+  sliderWidth = $list_img[0].offsetWidth
+  
+})
+let currentIndex = 0;
+let sliderCount = $list_img.length;
+
+$head_next.addEventListener("click", ()=>{
+  $popzon.style.cssText = `margin-left:-${sliderWidth}px; transition:0.5s;`
+  setTimeout(()=>{
+    $popzon.appendChild($popzon.firstElementChild)
+    $popzon.style.cssText = `margin-left:0px;`;
+  },500)
+let nextIndex = (currentIndex + 1) % sliderCount;
+
+currentIndex = nextIndex;
+})
+$head_prev.addEventListener("click",()=>{
+  $popzon.prepend($popzon.lastElementChild)
+  $popzon.style.cssText = `margin-left:-${sliderWidth}px`
+  setTimeout(()=>{
+    $popzon.style.cssText = `margin-left:0px; transition:0.5s;`
+  })
+})
+
