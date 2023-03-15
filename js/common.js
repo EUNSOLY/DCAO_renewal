fc06a37a0524f5e3b8e6f51febe5f162e0864dc8
EUNSOLY <dmsthf9596@gmail.com>
2023-03-15 18:00:24 +0900

    23.03.15 대전시민천문대 리뉴얼 6일차

--- js/common.js
+++ js/common.js
@@ -15,7 +15,7 @@ const $gnb_list = document.querySelectorAll(".gnb_list");
 $gnb_list.forEach((item, i) => {
   item.addEventListener("click", () => {
     $gnb_list.forEach((item, i) => {
-      item.classList.remove("on");
+      item.classList.removegit("on");
     });
 
     item.classList.add("on");
