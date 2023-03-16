3f30d95eee6a46b9dec30c104e4e42b0cef82de5
EUNSOLY <dmsthf9596@gmail.com>
2023-03-16 20:01:22 +0900

    23.03.16 대전시민천문대 리뉴얼 7일차

--- js/prosses.js
+++ js/prosses.js
@@ -14,3 +14,48 @@ tapBtn.forEach((item, i) => {
     item.classList.add("on");
   });
 });
+
+
+// 개인 단체 선택 구현
+const prosses_choice = document.querySelector(".prosses_choice")
+const choice = prosses_choice.querySelectorAll("input")
+
+
+const individual = document.querySelector(".individual")
+const group = document.querySelector(".group")
+
+
+
+const processes = document.querySelectorAll('.processes')
+console.log(processes);
+
+
+
+const Btn = document.querySelectorAll(".processes_1Btn > input")
+console.log(Btn);
+
+
+Btn.forEach((item,i )=>{
+  item.addEventListener("click", ()=>{
+    processes.forEach((item)=>{
+      item.classList.remove("on")
+    })
+    processes[i+1].classList.add("on")
+  })
+
+ 
+  
+})
+const modal = document.querySelector(".reservationModal")
+const modalBtn = document.querySelector(".modal_con input")
+let arrayLast = Btn[Btn.length - 1]
+
+// console.log(arrayLast);
+arrayLast.addEventListener("click", ()=>{
+console.log("마지막이다");
+modal.classList.add("on")
+})
+modalBtn.addEventListener("click", ()=>{
+  modal.classList.remove("on")
+  
+})
